e7888a643876a9a28728bb843a810284af9632d1
CC <CC@CC-001-025>
2012-11-14 09:13:27 +0000

    Day commit

--- EGitTest/src/HelloGit.java
+++ EGitTest/src/HelloGit.java
@@ -5,7 +5,7 @@ public class HelloGit {
 		System.out.println("Hello git!");
 		System.out.println("Hello git!");
 		System.out.println("Hello git!");
-
+		System.out.println("Hello git! (X)");
 
 	}
 }
